26f697fda343c17f50dbd4d6d94c159f691c879f
Ken Andrews <paddlebike@yahoo.com>
2017-07-31 17:00:36 -0400

    Completed chapter 10b using arguments in the bundle

--- app/src/main/java/com/scratch/kena/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/scratch/kena/criminalintent/CrimeListFragment.java
@@ -41,12 +41,24 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if (mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
+
+
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder
@@ -72,6 +84,7 @@ public class CrimeListFragment extends Fragment {
             Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
         }
+
 
         public void bind(Crime crime) {
             mCrime = crime;
