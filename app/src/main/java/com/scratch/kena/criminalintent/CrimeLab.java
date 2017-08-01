29bc4617d670410d9532ccaf9bed622f507663a6
Ken Andrews <paddlebike@yahoo.com>
2017-08-01 13:38:58 -0400

    Completed chapter 13 Added a menu bar

--- app/src/main/java/com/scratch/kena/criminalintent/CrimeLab.java
+++ app/src/main/java/com/scratch/kena/criminalintent/CrimeLab.java
@@ -22,15 +22,9 @@ public class CrimeLab {
         return sCrimeLab;
     }
 
-    private CrimeLab(Context context){
-        mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setIsSolved(i % 2 == 0);  // every other one
-            mCrimes.add(crime);
-        }
-    }
+    private CrimeLab(Context context){  mCrimes = new ArrayList<>(); }
+
+    public void addCrime(Crime crime){ mCrimes.add(crime);  }
 
     public List<Crime> getCrimes() {
         return mCrimes;
