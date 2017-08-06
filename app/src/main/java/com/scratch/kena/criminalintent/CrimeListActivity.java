bc1e05dd02b3bc70e129cde4f7b1fcaece779f7c
Ken Andrews <paddlebike@yahoo.com>
2017-08-06 17:28:50 -0400

    broken adding tablet support

--- app/src/main/java/com/scratch/kena/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/scratch/kena/criminalintent/CrimeListActivity.java
@@ -1,6 +1,7 @@
 package com.scratch.kena.criminalintent;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 
 import java.util.UUID;
@@ -8,11 +9,33 @@ import java.util.UUID;
  * Created by kena on 7/31/17.
  */
 
-public class CrimeListActivity extends SingleFragmentActivity {
-
+public class CrimeListActivity extends SingleFragmentActivity
+    implements CrimeListFragment.Callbacks {
 
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
     }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_masterdetail;
+    }
+
+    @Override
+    public void onCrimeSelected(Crime crime) {
+        if (findViewById(R.id.detail_fragment_container) == null) {
+            Intent intent = CrimePagerActivity.newIntent(this, crime.getId());
+            startActivity(intent);
+        } else {
+            Fragment newDetail = CrimeFragment.newInstance(crime.getId());
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail_fragment_container, newDetail)
+                    .commit();
+        }
+    }
+
 }
+
+
--- app/src/main/java/com/scratch/kena/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/scratch/kena/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.scratch.kena.criminalintent;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -31,6 +32,22 @@ public class CrimeListFragment extends Fragment {
     private static final String SAVED_SUBTITLE_VISIBLE = "subtitle";
     private static int REQUEST_CRIME = 1;
     private boolean mSubtitleVisible;
+    private Callbacks mCallbacks;
+
+    /**
+     * required interface for hosting activities
+     *
+     */
+
+    public interface Callbacks {
+        void onCrimeSelected(Crime crime);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mCallbacks = (Callbacks) context;
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -79,6 +96,12 @@ public class CrimeListFragment extends Fragment {
     }
 
     @Override
+    public void onDetach() {
+        super.onDetach();
+        mCallbacks = null;
+    }
+
+    @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_crime_list, menu);
@@ -97,8 +120,8 @@ public class CrimeListFragment extends Fragment {
             case R.id.new_crime:
                 Crime crime = new Crime();
                 CrimeLab.get(getActivity()).addCrime(crime);
-                Intent intent = CrimePagerActivity.newIntent(getActivity(), crime.getId());
-                startActivity(intent);
+                updateUI();
+                mCallbacks.onCrimeSelected(crime);
                 return true;
             case R.id.show_subtitle:
                 mSubtitleVisible = !mSubtitleVisible;
@@ -157,8 +180,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
-            startActivityForResult(intent, REQUEST_CRIME);
+            mCallbacks.onCrimeSelected(mCrime);
         }
 
 
